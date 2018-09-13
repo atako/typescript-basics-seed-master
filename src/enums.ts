59eea00b7105c272ed4463b9e533341bf98b1dc4
Dmitry Ryumkin <riumkin@gmail.com>
2018-09-14 09:42:41 +1000

    enums

---
+++ src/enums.ts
@@ -0,0 +1,14 @@
+enum Sizes {
+  Small = "small",
+  Medium = "medium",
+  Large = "large"
+}
+
+let selected: Sizes;
+
+function updateSize(size: Sizes): void {
+  selected = size;
+}
+
+updateSize(Sizes.Large);
+
